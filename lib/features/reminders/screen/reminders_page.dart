fdfd3fbcda2791511b9aabde5f52f99ccebdf592
rujankhiuju <rujankhiuju@gmail.com>
2026-07-01 01:08:19 +0000

    fix: correct relative imports in reminders_page.dart (../../ -> ../../../)

--- lib/features/reminders/screen/reminders_page.dart
+++ lib/features/reminders/screen/reminders_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../core/theme/app_colors.dart';
-import '../../shared/widgets/staggered_animation.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../shared/widgets/staggered_animation.dart';
 import '../model/reminder.dart';
 import '../providers/reminder_provider.dart';
 
